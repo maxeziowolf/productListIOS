103f4074003f0cb08dea84b218ba7c3b95b2bc78
maxeziowolf <maxeziowolf@outlook.com>
2022-05-26 23:50:13 -0500

    Finished first view

--- ProductoList/Controllers/ViewController.swift
+++ ProductoList/Controllers/ViewController.swift
@@ -9,14 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    //MARK:
     @IBOutlet weak var lblName: UILabel!
     @IBOutlet weak var lblCategory: UILabel!
     @IBOutlet weak var lblPriceRegular: UILabel!
     @IBOutlet weak var lblPriceFinal: UILabel!
+    @IBOutlet weak var lblDescription: UILabel!
     @IBOutlet weak var imageProduct: UIImageView!
+    @IBOutlet weak var imageDescont: UIImageView!
     @IBOutlet weak var btnShowList: UIButton!
     @IBOutlet weak var cardProduct: UIView!
+    @IBOutlet weak var containerCard: UIView!
     
+    
+    //Variable
+    private let timeDelay = 3.0
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -64,33 +71,49 @@ extension ViewController: ListProductsDelegate{
     
     func setInformation(product: Product) {
         
+        containerCard.isHidden = false
+        
+        lblDescription.text = "El producto es el siguiente: "
         
         imageProduct.downloadedFrom(link: product.urlImagenes[0], contentMode: .scaleAspectFit, animated: true)
         
-        lblName.text = product.nombre.capitalized
+        lblName.setTextWithTypeAnimation(typedText: product.nombre.capitalized, characterDelay: timeDelay)
         
-        lblCategory.text = "\(product.category.capitalized) (\(product.codigoCategoria!))"
+        lblCategory.setTextWithTypeAnimation(typedText: "\(product.category.capitalized) (\(product.codigoCategoria!))" , characterDelay: timeDelay)
         
         if product.descuento {
             
             lblPriceRegular.isHidden = false
             lblPriceFinal.isHidden = false
+            imageDescont.isHidden = false
             
-            lblPriceRegular.text = product.precioRegular.asMoney()
-            lblPriceFinal.text = product.precioFinal.asMoney()
+            UIView.animate(withDuration: 0.1, delay: 0.1, options: .curveLinear, animations: {
+                
+                self.imageDescont.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
+                        
+            }) { (success) in
+                UIView.animate(withDuration: 0.1, delay: 0.0, options: .curveLinear, animations: {
+                    
+                    self.imageDescont.transform = .identity
+                    
+                }, completion: nil)
+            }
+            
+            lblPriceRegular.setTextWithTypeAnimation(typedText: product.precioRegular.asMoney(), characterDelay: timeDelay)
+            
+            lblPriceFinal.setTextWithTypeAnimation(typedText: product.precioFinal.asMoney(), characterDelay: timeDelay)
             
         }else{
             
             lblPriceRegular.isHidden = true
             lblPriceFinal.isHidden = false
+            imageDescont.isHidden = true
             
-            lblPriceFinal.text = product.precioRegular.asMoney()
+            lblPriceFinal.setTextWithTypeAnimation(typedText: product.precioRegular.asMoney(), characterDelay: timeDelay)
             
         }
+        
     }
-    
-    
-    
     
 }
 
@@ -152,5 +175,26 @@ extension Double{
             return "\(self)"
         }
     }
+}
+
+extension UILabel {
+    func setTextWithTypeAnimation(typedText: String, characterDelay: TimeInterval = 5.0) {
+        text = ""
+        var writingTask: DispatchWorkItem?
+        writingTask = DispatchWorkItem { [weak weakSelf = self] in
+            for character in typedText {
+                DispatchQueue.main.async {
+                    weakSelf?.text!.append(character)
+                }
+                Thread.sleep(forTimeInterval: characterDelay/100)
+            }
+        }
+        
+        if let task = writingTask {
+            let queue = DispatchQueue(label: "typespeed", qos: DispatchQoS.userInteractive)
+            queue.asyncAfter(deadline: .now() + 0.05, execute: task)
+        }
+    }
+    
 }
 
